81150053ee9e3c52718c39cd3b56d70995c909d2
Ethan Seiber <ethcseib@ut.utm.edu>
2020-02-11 18:54:24 -0600

    Finished project

--- sudoku.c
+++ sudoku.c
@@ -6,25 +6,22 @@
 typedef struct{
   int row;
   int column;
-  //int** graph;
   int graph[10][25];
-  int check;
 }parameters;
 
 void* ColumnCheck(void*);
 void* RowCheck(void*);
 void* SubgridCheck(void*);
+void FillParameters(parameters*, parameters*);
 
-char* FormatGraph(char*);
-void Concatenate(char*, char*);
-
+int checks = 0;
 int main(int argc, char* argv[]){
-
+  
   if(argc > 1){
     FILE* fptr = fopen(argv[1], "r");
     char table[10][25];
     int i = 1;
-    parameters *param = (parameters*) malloc(sizeof(parameters));
+    parameters* param = (parameters*) malloc(sizeof(parameters));
 
     while(fgets(table[i], 25, fptr) != NULL){
       i+=1;
@@ -39,7 +36,7 @@ int main(int argc, char* argv[]){
 	  param->graph[j][i] = table[j][k]-'0';
 	  i += 1;
 
-	  if(param->check != 0){
+	  if(checks != 0){
 	    printf("\nThe sudoku table is wrong.\n");
 	    exit(0);
 	  }
@@ -51,15 +48,10 @@ int main(int argc, char* argv[]){
     pthread_t columns;
     pthread_t rows;
 
-    /*for(int i = 1; i < 10; i++){
-      printf("param->graph[%d][%d] =  %d\n",i,9,param->graph[i][0]);
-      }*/
-
-    param->check = 0;
     param->row = 1;
     param->column = 1;
     
-    /*for(int i = 0; i < 9; i++){
+    for(int i = 0; i < 9; i++){
       
       pthread_create(&columns, NULL, ColumnCheck,(void*) param);
       pthread_create(&rows, NULL, RowCheck,(void*) param);
@@ -67,53 +59,54 @@ int main(int argc, char* argv[]){
       pthread_join(columns,NULL);
       pthread_join(rows, NULL);
       
-      //printf("check %d\n", param->check);
-      if(param->check != 0){
-	printf("\nThe sudoku table is wrong.\n");
-	exit(0);
-      }
       param->row += 1;
       param->column += 1;
-      }*/
+    }
     
-    //free(param);
-
+    pthread_t subgrid[10];
+    parameters* tmp = (parameters*) malloc(10*sizeof(parameters));
+    
     param->row = 1;
     param->column = 1;
-    pthread_t subgrid[10];
-    //FILE*fptr = fopen("main.txt", "w");
-    FILE* fpt;
-    for(int i = 0; i < 9; i++){ //<= 8; i++){
+    
+    for(int i = 0; i < 9; i++){
+      
+      FillParameters(tmp, param);
 
-      printf("\n\ncolumn %d row %d\n\n", param->column, param->row);
-      printf("double %d", param->column);
-      //SubgridCheck((void*) param);
-      pthread_create(&subgrid[i], NULL, SubgridCheck, (void*) param);
-      //pthread_join(subgrid[i], NULL);
-      //SubgridCheck((void*)param);
+      pthread_create(&subgrid[i], NULL, SubgridCheck, (void*) tmp);
+      tmp++;
+      
       param->column += 3;
       
       if(param->column >= 9){
 	param->column = 1;
 	param->row += 3;
-      }
-
-      /*if(param->check != 0){
-	printf("\nThe sudoku table is wrong.\n");
-	exit(0);
-	}*/
+      }    
     }
-    for(int i = 0; i < 9; i++){//<= 8; i++){
+    for(int i = 0; i < 9; i++){
       pthread_join(subgrid[i], NULL);
     }
-
-    if(param->check != 0){                                                  
-      printf("\nThe sudoku table is wrong.\n");                               
-      exit(0);                                                                
+    if(checks != 0){
+	printf("\nThe sudoku table is wrong.\n");
     }
-    free(param); 
+    else{
+      printf("The Sudoku table is correct\n");
+    }
+    free(param);
+    //free(tmp);//Cant free memmory.   
   }  
   exit(0);
+}
+
+void FillParameters(parameters* fill, parameters* with){
+
+  fill->row = 0;
+  fill->column = 0;
+ 
+  for(int x = 0; x < 4; x++)
+    for(int y = 0; y < 4; y++){
+      fill->graph[x][y] = with->graph[with->row + x][with->column + y];
+    }
 }
 
 void* ColumnCheck(void* param){
@@ -124,6 +117,7 @@ void* ColumnCheck(void* param){
   for(int i = 0; i < 10; i++){
     arr[i] = 0;
   }
+
   for(int i = 1; i < 10; i++){
 
     if(arr[param1->graph[i][param1->column]] != 2){
@@ -131,7 +125,7 @@ void* ColumnCheck(void* param){
       arr[param1->graph[i][param1->column]] = 2;
     }
     else{
-      param1->check = 1;
+      checks = 1;
     }
   }
   free(arr);
@@ -154,7 +148,7 @@ void* RowCheck(void* param){
       }
       else{
 	
-        param1->check = 1;
+        checks = 1;
       }
   }
 }
@@ -163,29 +157,20 @@ void* SubgridCheck(void* tmp){
 
   parameters* param = tmp;
   int arr[10];
-  //int j = 0;
-  FILE* fptr = fopen("chech.txt", "w");
+
   for(int i = 0; i < 10; i++){
     arr[i] = 0;
   }
-
-  fputs("rows and cols\n",fptr);
-  fputc(param->row + '0', fptr);fputs(" ",fptr);fputc(param->column + '0', fptr);
-  for(int x = param->row; x < param->row + 3; x++){
-    for(int y = param->column; y < param->column + 3; y++){
-      fputs(" param ",fptr);fputc(param->graph[x][y] +'0', fptr);
-    }fputs("\n",fptr);
-  }
-  //printf("\n\nbef\n\n");
+ 
   for(int x = param->row; x < param->row + 3; x++){
     for(int y = param->column; y < param->column + 3; y++){
 
-      //printf("param->graph[%d][%d] = %d\n",x,y,param->graph[x][y]);
       if(arr[param->graph[x][y]] != 2){
+	
         arr[param->graph[x][y]] = 2;
       }
       else{
-	param->check = 1;//param->graph[x][y];
+	checks = 1;
       }
     }
   }
