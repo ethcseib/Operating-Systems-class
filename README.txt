3bd9748978370375930194837f08b93e6d3d9ff5
Ethan Seiber <ethcseib@ut.utm.edu>
2020-02-12 22:17:42 +0000

    Fully documented project

---
+++ README.txt
@@ -0,0 +1,20 @@
+Ethan Seiber and Cody Ginowski
+
+Design Overview:
+       Created a typedef struct for passing parameters to threads.
+
+       Created the function FillParameters to avoid race conditions when looking at the 3*3 subgrids.
+
+       The program consists of three driving loops: an input loop that reads a sudoku table from a file,
+       a row and column checking loop, then a loop for checking 3*3 subgrids for correctness.
+
+       Used a global variable called check that threads have access to. This variable is used to signal
+       whether the sudoku table provided is correct.
+
+       There are a total of 3 sudoku table checking functions. One function for checking the table
+       columns, another for the rows and lastly one for checking a subgrid.
+       
+       Designed the program to be used specifically for a 9*9 sudoku table and thus doesn't scale. 
+
+Known Bugs:
+      There are no known bugs and all things were implemented that needed to be.
--- sudoku.c
+++ sudoku.c
@@ -1,19 +1,48 @@
+/**
+ *Ethan Seiber and Cody Ginowski
+ *Date: 2/12/2020 
+ *File: sudoku.c
+ *Description: A program that takes a filename containing a 9*9 sudoku table. It checks the table for correctness.
+ */
 #include<pthread.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
+/**
+ *The parameters to be passed to threads
+ */
 typedef struct{
   int row;
   int column;
   int graph[10][25];
 }parameters;
 
+/**
+ *Function: ColumnCheck
+ *Description: Gets passed to a thread. Checks a column of the sudoku table for correctness.
+ */
 void* ColumnCheck(void*);
+
+/**
+ *Function: RowCheck
+ *Description: Gets passed to a thread. Checks a sudoku table row for corectness.
+ */
 void* RowCheck(void*);
+
+/**
+ *Function: SubgridCheck
+ *Description: Gets passed to a thread. Checks a 3*3 subgrid of the sudoku table for correctness.
+ */
 void* SubgridCheck(void*);
+
+/**
+ *Function: FillParameters
+ *Description: Fills a parameter pointer with a subgrid to be passed to the SubgridCheck.
+ */
 void FillParameters(parameters*, parameters*);
 
+//Used to determine whether a sudoku table is correct.
 int checks = 0;
 int main(int argc, char* argv[]){
   
@@ -31,6 +60,9 @@ int main(int argc, char* argv[]){
     fclose(fptr);
     
     i = 1;
+    /**
+     *Translates the sudoku table read from the file to integers for future reading.
+     */
     for(int j = 0; j < 10; j++){
       for(int k = 0; k < 18; k++){
 	if(k%2 == 0){
@@ -52,7 +84,9 @@ int main(int argc, char* argv[]){
 
     param->row = 1;
     param->column = 1;
-    
+    /**
+     *This loop performs the rows and columns checks.
+     */
     for(int i = 0; i < 9; i++){
       
       pthread_create(&columns, NULL, ColumnCheck,(void*) param);
@@ -64,13 +98,18 @@ int main(int argc, char* argv[]){
       param->row += 1;
       param->column += 1;
     }
-    
+
+    //An array of threads.
     pthread_t subgrid[10];
+
+    //An array of parameters where each element contains a
+    //different subgird of the sudoku table avoiding race conditions.
     parameters* tmp = (parameters*) malloc(10*sizeof(parameters));
     
     param->row = 1;
     param->column = 1;
-    
+
+    //This loop looks at all 9 subgrids and checks for correctness.
     for(int i = 0; i < 9; i++){
       
       FillParameters(tmp, param);
@@ -108,6 +147,10 @@ int main(int argc, char* argv[]){
   exit(0);
 }
 
+/**                                                                                                     
+ *Function: FillParameters                                                                              
+ *Description: Fills a parameter pointer with a subgrid to be passed to the SubgridCheck.               
+ */
 void FillParameters(parameters* fill, parameters* with){
 
   fill->row = 0;
@@ -119,6 +162,10 @@ void FillParameters(parameters* fill, parameters* with){
     }
 }
 
+/**                                                                                                     
+ *Function: ColumnCheck                                                                                
+ *Description: Gets passed to a thread. Checks a column of the sudoku table for correctness.
+ */
 void* ColumnCheck(void* param){
 
   parameters* param1 = param;
@@ -141,6 +188,10 @@ void* ColumnCheck(void* param){
   free(arr);
 }
 
+/**                                                                                                     
+ *Function: RowCheck                                                                                    
+ *Description: Gets passed to a thread. Checks a sudoku table row for corectness.                       
+ */
 void* RowCheck(void* param){
 
   parameters *param1 = param;
@@ -163,6 +214,10 @@ void* RowCheck(void* param){
   }
 }
 
+/**                                                                                                     
+ *Function: FillParameters                                                                              
+ *Description: Fills a parameter pointer with a subgrid to be passed to the SubgridCheck.               
+ */
 void* SubgridCheck(void* tmp){
 
   parameters* param = tmp;
