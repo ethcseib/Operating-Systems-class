c8675417c8436997778766ad2b1a83a831d42df5
Ethan Seiber <ethcseib@ut.utm.edu>
2020-02-15 22:10:12 +0000

    Finished project

--- 3/banking.c
+++ 3/banking.c
@@ -1,13 +1,13 @@
-#define N 100
 typedef int semaphore;
 semaphore mutex = 1;
-semaphore empty = N;
-semaphore full = 0;
+semaphore check = 0;
 
 #include<pthread.h>
 #include<stdio.h>
 #include<unistd.h>//needed for usleep
 
+pthread_mutex_t the_mutex;
+pthread_cond_t condW;
 int balance = 0;
 
 void* Withdraw(void*);
@@ -15,7 +15,9 @@ void* Deposit(void*);
 
 int main(int argc, char* argv[]){
 
-pthread_t th[4];
+pthread_t th[4];//4
+pthread_mutex_init(&the_mutex, NULL);
+pthread_cond_init(&condW, NULL);
 
 for(int i = 0; i < 4; i++){
 
@@ -30,6 +32,12 @@ pthread_create(&th[i], NULL, Deposit, (void*)&th[i]);
 for(int i = 0; i < 4; i++){
 pthread_join(th[i], NULL);
 }
+
+pthread_mutex_destroy(&the_mutex);
+pthread_cond_destroy(&condW);
+
+printf("\nThe final balance is %d\n", balance);
+return(0);
 }
 
 void* Withdraw(void* tid){
@@ -37,11 +45,13 @@ void* Withdraw(void* tid){
   int* tmp = (int*) tid;
   
   for (int i=0; i<10; i++){
-    if(mutex == 1 && balance >= 10){//test
-      //crit reg starts
-      full -= 1;
+pthread_mutex_lock(&the_mutex);
       mutex -= 1;
-      int readbalance = balance;
+check += 1;
+if(balance <= 10){
+pthread_cond_wait(&condW, &the_mutex);
+}
+int readbalance = balance;
 printf("At time %d, the balance for withdrawal thread %d is %d\n", i, *tmp, balance);
       readbalance -= 10;
       usleep(1);
@@ -49,9 +59,9 @@ printf("At time %d, the balance for withdrawal thread %d is %d\n", i, *tmp, bala
 printf("At time %d, the balance after withdrawal thread %d is %d\n\n", i, *tmp, balance);
       
       //exit crit
+pthread_mutex_unlock(&the_mutex);
+check -= 1;
       mutex += 1;
-      empty += 1;
-    }  
     usleep(1); 
   } 
 }
@@ -61,21 +71,22 @@ void* Deposit(void* tid){
   int* tmp = (int*) tid;
   for (int i=0; i<10; i++){
     //enter crit
-    if(mutex == 1){//test
-    empty -= 1;
-    mutex -= 1;
+pthread_mutex_lock(&the_mutex);
 
     int readbalance = balance;
 printf("At time %d, the balance before depositing thread %d is %d\n", i, *tmp, balance);
+
+if(balance >= 10){
+pthread_cond_signal(&condW);
+}
     readbalance += 11;
     usleep(10);
     balance = readbalance;
 printf("At time %d, the balance after depositing thread %d is %d\n\n", i, *tmp, balance);
 
     //exit crit
-    mutex += 1;
-    full += 1;
-    }
-    usleep(10);
-  }
+pthread_mutex_unlock(&the_mutex);
+
+usleep(10);
+}
 }
