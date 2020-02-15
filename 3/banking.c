d9f01bf3506e65a99f1bc569ad366cd9daf0f6c1
Ethan Seiber <ethcseib@ut.utm.edu>
2020-02-14 23:41:29 -0600

    Progress

--- 3/banking.c
+++ 3/banking.c
@@ -1,29 +1,81 @@
-#include<ptrhead.h>
-#include<stdio.h>
+#define N 100
+typedef int semaphore;
+semaphore mutex = 1;
+semaphore empty = N;
+semaphore full = 0;
 
-struct bank{
-  int balance;
-};
+#include<pthread.h>
+#include<stdio.h>
+#include<unistd.h>//needed for usleep
+
+int balance = 0;
 
 void* Withdraw(void*);
 void* Deposit(void*);
 
 int main(int argc, char* argv[]){
 
-  pthread_t with1;
-  pthread_t with2;
-  pthread_t dep1;
-  pthread_t dep2;
+pthread_t th[4];
 
+for(int i = 0; i < 4; i++){
+
+    if(i < 2){
+pthread_create(&th[i], NULL, Deposit, (void*)&th[i]);
+}
+    else{
+      pthread_create(&th[i], NULL, Withdraw, (void*)&th[i]);
+    }
+}
+
+for(int i = 0; i < 4; i++){
+pthread_join(th[i], NULL);
+}
+}
+
+void* Withdraw(void* tid){
+
+  int* tmp = (int*) tid;
   
+  for (int i=0; i<10; i++){
+    if(mutex == 1 && balance >= 10){//test
+      //crit reg starts
+      full -= 1;
+      mutex -= 1;
+      int readbalance = balance;
+printf("At time %d, the balance for withdrawal thread %d is %d\n", i, *tmp, balance);
+      readbalance -= 10;
+      usleep(1);
+      balance = readbalance;
+printf("At time %d, the balance after withdrawal thread %d is %d\n\n", i, *tmp, balance);
+      
+      //exit crit
+      mutex += 1;
+      empty += 1;
+    }  
+    usleep(1); 
+  } 
 }
 
-void* Withdraw(void* tmp){
-  struct bank* bnk = tmp;
-  bnk->balance -= 10;
-}
+void* Deposit(void* tid){
+  
+  int* tmp = (int*) tid;
+  for (int i=0; i<10; i++){
+    //enter crit
+    if(mutex == 1){//test
+    empty -= 1;
+    mutex -= 1;
 
-void* Deposit(void* tmp){
-  struct bank* bnk = tmp;
-  bnk += 11;
+    int readbalance = balance;
+printf("At time %d, the balance before depositing thread %d is %d\n", i, *tmp, balance);
+    readbalance += 11;
+    usleep(10);
+    balance = readbalance;
+printf("At time %d, the balance after depositing thread %d is %d\n\n", i, *tmp, balance);
+
+    //exit crit
+    mutex += 1;
+    full += 1;
+    }
+    usleep(10);
+  }
 }
