aa6e6164dc8a3c860df519908ac498f227b00f79
Ethan Seiber <ethcseib@ut.utm.edu>
2020-04-15 15:57:46 -0500

    Add files via upload

--- 1/Queue.c
+++ 1/Queue.c
@@ -23,3 +23,24 @@ char** pop(struct Queue* que){
 
   return tmp;
 }
+
+void pushPid(struct QueuePid* queue, pid_t* pid){
+  
+  queue->queue[queue->endQueue] = pid;
+  queue->size += 1;
+  queue->endQueue += 1;
+}
+
+pid_t* popPid(struct QueuePid* que){
+  
+  pid_t* tmp = que->queue[0];
+  for(int i = 1; i < que->endQueue; i++){
+
+    que->queue[i-1] = que->queue[i];
+  }
+  que->size -= 1;
+  que->queue[que->endQueue] = 0;
+  que->endQueue -= 1;
+
+  return tmp;
+}
